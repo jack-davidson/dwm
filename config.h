72270714fed0ec78f621f9fe51b192c3eea5fbb8
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-12 13:12:18 -0500

    updated the wm rules

--- config.h
+++ config.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,   0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,   1 << 6,       0,           -1 },
 	{ "qutebrowser",  NULL,   NULL,   1 << 6,       0,           -1 },
-	{ "Thunderbird",  NULL,   NULL,   1 << 7,       0,           -1 },
+	{ "Thunderbird",  NULL,   NULL,   1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
